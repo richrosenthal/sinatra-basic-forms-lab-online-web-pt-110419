db24ee9f5af239f6782f24bc815b7d597d4be12c
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-02-19 00:14:41 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,7 +1,7 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
 class Puppy 
   
-  attr_accessor :name, :breed, :assert_generates
+  attr_accessor :name, :breed, :age 
   
   def initialize(name, breed, age)
     
